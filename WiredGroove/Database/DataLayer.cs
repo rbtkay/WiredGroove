cb92ceaafaaa8dce02a1edff0bd78a7fed66fa06
robertkhayat <robert.g.khayat@gmail.com>
2018-12-28 16:25:09 +0200

    Database & interface

---
+++ WiredGroove/Database/DataLayer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WiredGroove.Database
+{
+    public class DataLayer : IDataLayer
+    {
+        string _connectionString;
+
+        public DataLayer(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+    }
+}
---
+++ WiredGroove/Database/IDataLayer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WiredGroove.Database
+{
+    public interface IDataLayer
+    {
+
+    }
+
+    public class DataLayerFactory
+    {
+        private static IDataLayer _instance;
+        private static object _locker = new object();
+
+        public static IDataLayer Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_locker)
+                    {
+                        if (_instance == null)
+                        {
+                            string connectionString = "Data Source=LAPTOP-HI71AFPV;Initial Catalog=WiredGrooveDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+                            _instance = new DataLayer(connectionString);
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+    }
+}
