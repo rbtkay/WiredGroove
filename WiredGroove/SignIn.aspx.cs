7f55e276583953352cede51b5b8748460ee0f2dc
KVNbog <kevin.boghossian@gmail.com>
2019-01-12 22:59:23 +0200

    chore: fix sign in

--- WiredGroove/SignIn.aspx.cs
+++ WiredGroove/SignIn.aspx.cs
@@ -19,8 +19,8 @@ namespace WiredGroove
         {
             if(DataLayerFactory.Instance.CheckAccount(txtEmailID.Text, txtPasswordID.Text))
             {
-                Response.Redirect("sHomePage.aspx");
                 Session["signInEmail"] = txtEmailID.Text;
+                Response.Redirect("sHomePage.aspx");
             }
             else
             {
